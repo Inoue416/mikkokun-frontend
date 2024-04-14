c21fd4a2bbcabb3988cb435637cfab59a817c175
井上優也 <inoueyuuya@inoueyuuyanonotobukkukonpyuta.local>
2024-04-14 22:16:08 +0900

    set biome fomatter and lintter

--- src/page.tsx
+++ src/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-	const message = "Hello World";
-	console.log(message);
+	// const a = 1;
+	let a = 1;
+	a++;
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
