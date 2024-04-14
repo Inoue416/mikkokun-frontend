dba105f255b432b9b20c18d8d3b28ecb5d0e9ea7
井上優也 <inoueyuuya@inoueyuuyanonotobukkukonpyuta.local>
2024-04-15 00:33:00 +0900

    [Edit] Confirm connected API server but http only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
-export default function Home() {
-	// const a = 1;
-	let a = 1;
-	a++;
+export default async function Home() {
+	console.log("fetch: ...");
+	const response = await fetch("http://localhost:8080/", { cache: 'no-store' });
+	const data =  await response.json();
+	console.log(data.message ?? "");
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
