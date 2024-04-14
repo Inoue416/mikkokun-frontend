6180bb22f7582776e405732d30ff2b0c9fd78b0b
井上優也 <inoueyuuya@inoueyuuyanonotobukkukonpyuta.local>
2024-04-14 22:05:58 +0900

    move files

--- src/page.tsx
+++ src/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 
 export default function Home() {
+	const message = "Hello World";
+	console.log(message);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
